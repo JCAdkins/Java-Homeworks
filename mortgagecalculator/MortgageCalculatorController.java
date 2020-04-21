58ddf937264f6dd4af1f2d7e50c0fc0e309e1245
anthony adkins <thebears5454@gmail.com>
2020-04-20 22:39:57 -0700

    Update CalculatePrice to take BigDec

--- mortgagecalculator/MortgageCalculatorController.java
+++ mortgagecalculator/MortgageCalculatorController.java
@@ -10,13 +10,13 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 
 public class MortgageCalculatorController {
-    
-        //formatters for currency and percentages
+
+    // formatters for currency and percentages
     private static final NumberFormat currency = NumberFormat.getCurrencyInstance();
     private static final NumberFormat percent = NumberFormat.getPercentInstance();
     private BigDecimal customLoanDuration = new BigDecimal(1.0);
-    
-    //FXML stuff
+
+    // FXML stuff
     @FXML
     private Label customLoanLabel;
 
@@ -25,7 +25,7 @@ public class MortgageCalculatorController {
 
     @FXML
     private TextField priceTextField;
-    
+
     @FXML
     private TextField interestRateTextField;
 
@@ -43,17 +43,15 @@ public class MortgageCalculatorController {
 
     @FXML
     private Label twentyYearLoanLabel;
-    
+
     @FXML
     private Label customLoanReportLabel;
 
-
     @FXML
     private void calculateButtonPressed(ActionEvent event) {
-        try 
-        {
-        
-            //Variables for different things I need
+        try {
+
+            // Variables for different things I need
             BigDecimal price = new BigDecimal(priceTextField.getText());
             BigDecimal basePrice = new BigDecimal(priceTextField.getText());
             BigDecimal downPayment = new BigDecimal(downPaymentTextField.getText());
@@ -61,21 +59,24 @@ public class MortgageCalculatorController {
             BigDecimal tenYears = new BigDecimal(120.0);
             BigDecimal twentyYears = new BigDecimal(240.0);
             BigDecimal thirtyYears = new BigDecimal(360.0);
-            
-            //Set the loan amount label then use calculatePrice() to determine the payment for 
-            //the different loan lengths and set the labels they correspond to
+
+            // Set the loan amount label then use calculatePrice() to determine the payment
+            // for
+            // the different loan lengths and set the labels they correspond to
             loanAmountLabel.setText(currency.format(price.subtract(downPayment)));
-            price = calculatePrice(basePrice.subtract(downPayment).doubleValue(), interest.doubleValue(), tenYears.doubleValue());
+            price = calculatePrice(basePrice.subtract(downPayment), interest, tenYears);
+
             tenYearLoanLabel.setText(currency.format(price) + "/month");
-            price = calculatePrice(basePrice.subtract(downPayment).doubleValue(), interest.doubleValue(), twentyYears.doubleValue());
+            price = calculatePrice(basePrice.subtract(downPayment), interest, twentyYears);
+
             twentyYearLoanLabel.setText(currency.format(price) + "/month");
-            price = calculatePrice(basePrice.subtract(downPayment).doubleValue(), interest.doubleValue(), thirtyYears.doubleValue());
+            price = calculatePrice(basePrice.subtract(downPayment), interest, thirtyYears);
+
             thirtyYearLoanLabel.setText(currency.format(price) + "/month");
-            price = calculatePrice(basePrice.subtract(downPayment).doubleValue(), interest.doubleValue(), customLoanDuration.doubleValue()*12);
+            price = calculatePrice(basePrice.subtract(downPayment), interest, customLoanDuration.multiply(new BigDecimal(12)));
+
             customLoanReportLabel.setText(currency.format(price) + "/month");
-        }
-        catch (NumberFormatException e)
-        {
+        } catch (NumberFormatException e) {
             // If user enters in nothing or something other than number
             priceTextField.setText("Enter Amount");
             priceTextField.selectAll();
@@ -84,40 +85,40 @@ public class MortgageCalculatorController {
             downPaymentTextField.selectAll();
             interestRateTextField.setText("Enter Rate");
             interestRateTextField.selectAll();
-                    
+
         }
 
     }
 
-     // called by FXMLLoader to initialize the controller
-  public void initialize() {
-      // 0-4 rounds down, 5-9 rounds up 
-      currency.setRoundingMode(RoundingMode.HALF_UP);
-      
-      // listener for changes to customLoadSlider's value
-      customLoanSlider.valueProperty().addListener(
-         new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> ov, 
-               Number oldValue, Number newValue) {
-               customLoanDuration = 
-                  BigDecimal.valueOf(newValue.intValue());
-               customLoanLabel.setText(customLoanDuration.toString() + " years");
-            }
-         }
-      );
-   }
-  
-  // Method returns the monthly price
-  public BigDecimal calculatePrice(double cPrincipal, double cInterest, double cNumMonths)
-  {
-      //Formula : M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1]     P=principle, n=number months, i=monthly interest rate
-            
-      cInterest = cInterest / 1200;
-      BigDecimal monthlyPrice = new BigDecimal(
-              (cPrincipal*(cInterest*Math.pow(1+cInterest, cNumMonths))) // numerator
-              /(Math.pow(1+cInterest,cNumMonths)-1));   //denominator
+    // called by FXMLLoader to initialize the controller
+    public void initialize() {
+        // 0-4 rounds down, 5-9 rounds up
+        currency.setRoundingMode(RoundingMode.HALF_UP);
 
-      return monthlyPrice;
-  }
+        // listener for changes to customLoadSlider's value
+        customLoanSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+                customLoanDuration = BigDecimal.valueOf(newValue.intValue());
+                customLoanLabel.setText(customLoanDuration.toString() + " years");
+            }
+        });
+    }
+
+    // Method returns the monthly price
+    public BigDecimal calculatePrice(BigDecimal cPrincipal, BigDecimal cInterest, BigDecimal cNumMonths) {
+        // Formula : M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1] P=principle, n=number
+        // months, i=monthly interest rate
+
+        
+        // how big should the scale be? 2? more than 2 decimal places for interest and the calculations inside? Im not sure.
+        //probably 2 for the monthly price divison/mulitplication tho
+        cInterest = cInterest.divide(divisor, 2, RoundingMode.HALF_UP);
+        BigDecimal monthlyPrice = new BigDecimal((cPrincipal
+                .multiply(cInterest.multiply(pow(1 + cInterest.doubleValue(), cNumMonths.doubleValue()), 2,
+                        RoundingMode.HALF_UP), 2, RoundingMode.HALF_UP) // numerator
+                .divide(Math.pow(1 + cInterest.doubleValue(), cNumMonths.doubleValue()) - 1, 2, RoundingMode.HALF_UP)) // denominator
+        );
+        return monthlyPrice;
+    }
 }
